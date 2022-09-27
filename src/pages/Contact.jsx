fa1cffa057b58bd655dded2b40156e12579cf0a1
Rafal Szostak <rafal.szostak2@gmail.com>
2022-09-27 18:55:04 +0200

    Item details

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,8 @@
+import { useState, useEffect } from "react"
+import { useParams, useSearchParams } from "react-router-dom"
+
+function Contact() {
+  return <div>Contact</div>
+}
+
+export default Contact
